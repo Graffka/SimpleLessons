d9ff528c81834b5af8890c347cbd19dce2636214
Graffka <82235854+Graffka@users.noreply.github.com>
2021-06-16 10:35:23 +0300

    Create L9_List.py
    List Simple

---
+++ L9_List.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------
+# Program by Vycheslav.H.
+#
+#
+# Version   Date    Info
+# 1.0       2021    Lesson
+#
+#------------------------------------------------------------------
+
+cars = ['Audi', 'BMW', 'Mercedes', 'VW']
+cars.append('Volvo')
+cars.insert(1, 'Scania')
+cars.insert(0, 'TEST')
