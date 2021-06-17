c21b22c9ab6ad6fe5f8d769f7d06b85638f24048
Graffka <82235854+Graffka@users.noreply.github.com>
2021-06-17 10:55:50 +0300

    ABC

---
+++ Class2.py
@@ -0,0 +1,8 @@
+from L10_Class import  Warior
+
+new_hero3=Warior("Guf", 10, 10)
+
+new_hero3.show_hero()
+new_hero3.move()
+
+
--- L10_Class.py
+++ L10_Class.py
@@ -32,6 +32,35 @@ class Warior():
     def set_heath(self, new_health):
         self.heath=new_health
 
+
+class SuperWarior(Warior):
+    """Child Warior"""
+    def __init__(self, login, level, skill, magic_level):
+        """Initiate our superwarior"""
+        super().__init__(login, level, skill)
+        self.magic_level = magic_level
+        self.magic = 100
+
+    def makemagic (self):
+        """Use magic"""
+        self.magic = self.magic - 10
+
+    def show_hero(self):
+        message_parametrs = (self.login + " Level is: " + str(self.level) + " Skill is: " + str(
+                             self.skill) + " Magic level is " + str(self.magic_level)+ " Health i"
+                             "s: " + str(self.heath) + " Magic is " + str(self.magic)).title()
+        print(message_parametrs)
+
+    @staticmethod
+    def age (level):
+
+        if level>5:
+            print("Hi level")
+
+
+
+
+
 new_hero1 = Warior("Guru", 1, 1)
 new_hero2 = Warior("Novice", 2, 2)
 
@@ -40,3 +69,7 @@ new_hero2.show_hero()
 new_hero1.move()
 new_hero2.set_heath(90)
 new_hero2.show_hero()
+super_hero1 = SuperWarior('Kronos', 10, 10, 10)
+super_hero1.show_hero()
+super_hero1.move()
+super_hero1.age(12)
